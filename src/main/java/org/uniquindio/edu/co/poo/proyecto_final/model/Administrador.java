6c2e0f82eac04a9dcfbc8bc6eff4bab81659a594
JuanLasso-Lint <Juane.lassob@uqvirtual.edu.co>
2025-11-15 23:26:31 -0500

    ..

--- src/main/java/org/uniquindio/edu/co/poo/proyecto_final/model/Administrador.java
+++ src/main/java/org/uniquindio/edu/co/poo/proyecto_final/model/Administrador.java
@@ -1,4 +1,11 @@
 package org.uniquindio.edu.co.poo.proyecto_final.model;
 
-public class Administrador {
+public class Administrador extends Persona{
+
+    private String idAdministrador;
+
+
+    public Administrador(String nombre, String identificacion, String telefono) {
+        super(nombre, identificacion, telefono);
+    }
 }
